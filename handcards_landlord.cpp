d9bffef0b35704dea23bedea0fb95ede3f7743ed
Xjy-9 <54631934+Xjy-9@users.noreply.github.com>
2020-07-03 18:23:23 +0800

    Update handcards_landlord.cpp

--- handcards_landlord.cpp
+++ handcards_landlord.cpp
@@ -31,3 +31,48 @@ void HandCardsLandlord::ClearPutCardList(){
     putCardGroup.setGroupCount(0);
     putCardGroup.setGroupValue(0);
 }
+
+//计算一组手牌的价值并返回
+HandCardsLandlord::HandCardValue HandCardsLandlord::countValue(){
+    HandCardValue handcardvalue;
+
+    if(this->HandCardCount==0){ //手牌已经打完
+        handcardvalue.sumValue = 0;
+        handcardvalue.neededRound = 0;
+        return handcardvalue; //返回手牌价值
+    }
+
+    HandType_DDZ handtype = this->getHandType(); //获得手牌的类型：未知或者单种牌型
+    if(handtype != HandType_DDZ::Unknown){ //如果只剩单种牌型
+        handcardvalue.sumValue = this->representPoint(handtype); //牌型的价值
+        handcardvalue.neededRound = 1; //只需要一次即可出完
+        return handcardvalue; //返回手牌价值
+    }
+
+    //无法一次打完
+    getPutCardList(); //获得要打出去的牌的最优解，结果放在成员数据中
+    CardGroup nowPutCardGroup = this->putCardGroup; //保存当前即将打出的牌
+    vector <int> nowPutCardList = this->value_PutCardType;
+
+    for(vector<int>::iterator iter = nowPutCardList.begin();
+        iter!=nowPutCardList.end(); iter++){
+        this->value_HandCardList[*iter]--;
+    }
+    this->HandCardCount -= nowPutCardGroup.getGroupCount();
+    HandCardValue tem_handcardvalue = countValue(); //递归剩余牌的价值
+
+    for(vector<int>::iterator iter = nowPutCardList.begin();
+        iter!=nowPutCardList.end(); iter++){
+        this->value_HandCardList[*iter]++;
+    }
+    this->HandCardCount += nowPutCardGroup.getGroupCount();
+
+    handcardvalue.sumValue = nowPutCardGroup.representPoint(nowPutCardGroup.getHandType())+ tem_handcardvalue.sumValue;
+    handcardvalue.neededRound = tem_handcardvalue.neededRound+1;
+    return handcardvalue; //返回手牌价值
+}
+
+void HandCardsLandlord::getPutCardList(){
+
+}
+
