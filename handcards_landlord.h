a6004e592abb559ca0e633614aa7078e16d8e269
Xjy-9 <54631934+Xjy-9@users.noreply.github.com>
2020-07-03 16:30:43 +0800

    Update handcards_landlord.h

--- handcards_landlord.h
+++ handcards_landlord.h
@@ -11,9 +11,14 @@
 class HandCardsLandlord : public HandCards
 {
     bool isWin; //是否获胜，即出完了
+    int sumValue; //总价值
+    int neededRound; //需要打几手牌
 public:
     HandCardsLandlord(Seat);
-
+    int HandCardValue(vector<CardDdz> cards); //计算一组手牌的价值并返回
+public:
+    CardGroup putCardGroup; //要打出去的牌型
+    void ClearPutCardList(); //清空要打出去的牌组
 public slots:
     std::vector<CardPicture*> PutCard(std::vector<CardDdz>); //出牌
 
