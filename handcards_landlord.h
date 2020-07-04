20e9cf72aa5709a3c6fc4289481710f8e7d45136
Xjy-9 <54631934+Xjy-9@users.noreply.github.com>
2020-07-04 16:06:15 +0800

    Update handcards_landlord.h

--- handcards_landlord.h
+++ handcards_landlord.h
@@ -11,21 +11,21 @@
 class HandCardsLandlord : public HandCards
 {
     bool isWin; //是否获胜，即出完了
-    int HandCardCount; //手牌张数
-    vector<CardDdz> handCardsLandlord; //牌
     struct HandCardValue{ //一副手牌的价值
         int sumValue; //总价值
         int neededRound; //需要打几手牌
     };
 public:
-    int value_HandCardList[18] = {0}; //记录每种类型的牌有多少张
-    vector <int> value_PutCardType; //记录要打出去的牌
-    HandCardsLandlord(Seat);
-    HandCardValue countValue(); //计算一组手牌的价值并返回
-public:
+    int value_HandCardList[56] = {0}; //记录每种类型的牌有多少张
     CardGroup putCardGroup; //要打出去的牌型
-    void ClearPutCardList(); //清空要打出去的牌组
-    void getPutCardList(); //获得要打出去的牌型
+    vector <int> value_PutCardList; //记录要打出去的牌
+
+    HandCardsLandlord(Seat);
+    void ClearPutCardList(); //在出牌前，先清空要打出去的牌组
+    void GetValueHandCardList(); //获取手牌中每种牌有多少张的信息，存放在value_HandCardList中
+    void InitHandCard(); //初始化手牌
+    HandCardValue countValue(); //计算一组手牌的价值并返回
+    void GetPutCardList_1(); //获得主动打出去的牌型的最优解
 public slots:
     std::vector<CardPicture*> PutCard(std::vector<CardDdz>); //出牌
 
